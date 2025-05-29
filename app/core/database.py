f65ab4f1668f4283d1d173e5e145f3196c6f6a8b
IraKolotilina <98360541+IraKolotilina@users.noreply.github.com>
2025-05-29 20:17:24 +0300

    3

--- app/core/database.py
+++ app/core/database.py
@@ -1,28 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from app.core.settings import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    # connect_args={"check_same_thread": False}  # если SQLite
-)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# чтобы SQLAlchemy “видела” все модели при инициализации
-import app.models.player
-import app.models.building
-import app.models.box
-import app.models.stable
-
-def init_db():
-    from app.models.base import Base
-    Base.metadata.create_all(bind=engine)
-
 def get_db():
-    """
-    FastAPI-зависимость: возвращает сессию и гарантирует её .close()
-    """
     db = SessionLocal()
     try:
         yield db
--- app/core/settings.py
+++ app/core/settings.py
@@ -1,17 +1,15 @@
-# app/core/settings.py
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore"       # <- игнорируем DB_USER, DB_PASSWORD и прочие из .env
-    )
-    DATABASE_URL: str
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
+    ALGORITHM: str
+    DATABASE_URL: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
-
-
-
